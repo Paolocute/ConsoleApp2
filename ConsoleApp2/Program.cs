2d0522738636bcc2013b91116e51b0828d488d88
delacruz.254328 <delacruz.254328@LAB-A-PC37>
2024-02-08 08:35:11 +0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp2
+{
+    internal class GreetingPrgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter the total number of your enrolled course: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the price of your favorite book: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+                
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Total enrolled courses: " + num);
+            Console.WriteLine("Price of my favorite book: " + num1);
+
+            Console.Write("\nPress any key to exit");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
